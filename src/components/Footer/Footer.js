5858c1294929b53ed6d53a1126e8f697a3ae5abc
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-05-19 22:47:49 +0300

    Add the link to my GitHub profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,30 @@ import Icon from '@mdi/react';
 import { mdiGithub } from '@mdi/js';
 
 const Footer = () => {
+  // return (
+  //   <div className="footer container">
+  //     <div className="footer content">
+  //       <div>rvolkov8</div>
+  //       <Icon path={mdiGithub} size={0.9} color="#f5f5f5" />
+  //     </div>
+  //   </div>
+  // );
+
   return (
     <div className="footer container">
-      <div className="footer content">
-        <div>rvolkov8</div>
-        <Icon path={mdiGithub} size={0.9} color="#f5f5f5" />
-      </div>
+      <a
+        href="https://github.com/rvolkov8"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="nickname">rvolkov8</div>
+        <Icon
+          className="github-icon"
+          path={mdiGithub}
+          size={0.9}
+          color="#f5f5f5"
+        />
+      </a>
     </div>
   );
 };
